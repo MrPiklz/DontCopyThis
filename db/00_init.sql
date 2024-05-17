e7a70bece529261bc9e35cca503f1c7022080dc1
Alex White <sannura@mail.ru>
2024-05-17 14:27:27 +0300

    added back creation replication slot

--- db/00_init.sql
+++ db/00_init.sql
@@ -1,3 +1,5 @@
+select pg_create_physical_replication_slot('replication_slot');
+
 CREATE TABLE mails_s (id  SERIAL PRIMARY KEY, mails_s text);
 CREATE TABLE phone_num (id  SERIAL PRIMARY KEY, phone_num VARCHAR(100));
 
