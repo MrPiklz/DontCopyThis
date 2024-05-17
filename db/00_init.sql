a41da551e4fe5bf931cbfe588048448fa9d6b306
Alex White <sannura@mail.ru>
2024-05-17 14:22:29 +0300

    changes in init sql
    deleted repl_user

--- db/00_init.sql
+++ db/00_init.sql
@@ -1,6 +1,3 @@
-create user replicator with replication encrypted password 'replicator_password';
-select pg_create_physical_replication_slot('replication_slot');
-
 CREATE TABLE mails_s (id  SERIAL PRIMARY KEY, mails_s text);
 CREATE TABLE phone_num (id  SERIAL PRIMARY KEY, phone_num VARCHAR(100));
 
